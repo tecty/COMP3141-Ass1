c643bc84877d16327ca2955feeab8b8e40f12e04
Toby HUANG <z5141448@ad.unsw.edu.au>
2019-07-03 19:55:21 +1000

    exercise 1 is done

--- TortoiseCombinators.hs
+++ TortoiseCombinators.hs
@@ -12,7 +12,13 @@ import Tortoise
 -- of these combinators.
 
 andThen :: Instructions -> Instructions -> Instructions
-andThen i1 i2 = error "'andThen' unimplemented"
+andThen (SetStyle l i1)  i2    = SetStyle  l  $ andThen i1 i2
+andThen (PenUp i1)       i2    = PenUp        $ andThen i1 i2
+andThen (SetColour c i1) i2    = SetColour c  $ andThen i1 i2
+andThen (Move m i1)      i2    = Move      m  $ andThen i1 i2
+andThen (PenDown i1)     i2    = PenDown      $ andThen i1 i2
+andThen (Turn r i1)      i2    = Turn      r  $ andThen i1 i2
+andThen (Stop)           i2    = i2
 
 loop :: Int -> Instructions -> Instructions
 loop n i = error "'loop' unimplemented"
